a38d02e2b6d80f19e9a3a87e303a75d42f080f61
saeedrb <saeed.rb71@gmail.com>
2025-10-22 17:11:26 +0330

    change _layout.tsx component name

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { I18nManager } from "react-native";
 
-export default function HomeLayout() {
+export default function RootLayout() {
   useEffect(() => {
     I18nManager.allowRTL(true);
     I18nManager.forceRTL(true);
