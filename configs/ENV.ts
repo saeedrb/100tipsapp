03d52fdee1681f17b956a9697e5664506f5a1045
saeedrb <saeed.rb71@gmail.com>
2025-10-26 13:33:02 +0330

    empty configs

--- configs/ENV.ts
+++ configs/ENV.ts
@@ -1,7 +1,7 @@
 const ENV = {
   SUPABASE_ANON_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indtc3NxZmxzbGxrdGJ3eHBuc2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2NjUwNTIsImV4cCI6MjA3MzI0MTA1Mn0.ojmyykOLhvvWWT2h90hof26VEgXOIJC6_AB9SHCnO-0",
-  SUPABASE_URL: "https://wmssqflsllktbwxpnsed.supabase.co",
+    "",
+  SUPABASE_URL: "",
 };
 
 export default ENV;
